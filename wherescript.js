07ecf5047e3f0592071bbbecf6711f134392a2bb
Harrison Wynn <harrisonwynn42@gmail.com>
2017-04-01 19:11:40 -0500

    the web application collects more data, but requires input from the user before it can begin. This will be used by our location logger(s) when finished. Next step is to set this up to communicate to a mysql database.
    Signed-off-by: Harrison Wynn <harrisonwynn42@gmail.com>

--- wherescript.js
+++ wherescript.js
@@ -9,23 +9,26 @@ function check(){
 	var gender = document.querySelector('input[name="gender"]:checked').value;
 	var handicap = document.querySelector('input[name="handicap"]:checked').value;
 	var floor = document.getElementById("floor").value;
-	if((gender=="none")||(handicap=="none")||(floor=="none")){console.log("nope");}
-		//document.getElementById("findme").disabled = true;
-		
-		console.log("gender: " + document.querySelector('input[name="gender"]:checked').value + " var: " + gender);
-		console.log("handicap: " + document.querySelector('input[name="handicap"]:checked').value + " var: " + handicap);
-		console.log("floor: " + document.getElementById("floor").value + " var: " + floor);
-	//}
-	//else{
-		//document.getElementById("findme").disabled = false;
-	//	console.log("ready");
-	//}
+	if((gender=="none")||(handicap=="none")||(floor=="none")){
+		document.getElementById("findme").disabled = true;
+	}
+	else{
+		document.getElementById("findme").disabled = false;
+	}
 }
 
 function tellMe(){
-	console.log("gender: " + document.querySelector('input[name="gender"]:checked').value);
-	console.log("handicap: " + document.querySelector('input[name="handicap"]:checked').value);
-	console.log(document.getElementById("floor").value);
+	var gender = document.querySelector('input[name="gender"]:checked').value;
+	
+	var handicap;
+	if (document.querySelector('input[name="handicap"]:checked').value == "yes")
+	{handicap = "true";}
+	else
+	{handicap = "false";}
+
+	var floor = document.getElementById("floor").value;
+	
+	console.log(gender + " " + handicap + " " + floor);
 }
 				
 function findMe(){
@@ -34,7 +37,7 @@ function findMe(){
 		navigator.geolocation.getCurrentPosition(function(my_position){
 			//we're using an XMLHttpRequest!
 			//http://www.openjs.com/articles/ajax_xmlhttp_using_post.php
-			xmlhttp=new XMLHttpRequest();
+			//xmlhttp=new XMLHttpRequest();
 			//variable name vN
 			//variable data vD
 			//these will be packaged and sent into the xmlhttp request
@@ -46,14 +49,39 @@ function findMe(){
 			var	vD_A = my_position.coords.accuracy.toString();
 			var vN_T = "t=";
 			var	vD_T = my_position.timestamp.toString();
-				
+			
+			var vN_AL = "alt=";
+			var vD_AL = my_position.coords.altitude.toString();
+			var vN_AA = "altaccy=";
+			var vD_AA = my_position.coords.altitudeAccuracy.toString();
+			
+			var vN_G = "gdr";
+			var vD_G = document.querySelector('input[name="gender"]:checked').value.toString();
+			var vN_H = "han";
+			var vD_H;
+			if (document.querySelector('input[name="handicap"]:checked').value == "yes")
+			{vD_H = "true";}
+			else
+			{vD_H = "false";}
+			var vN_F = "flr";
+			var vD_F = document.getElementById("floor").value.toString();
+			
+			
+			
 			var url = "places.php";
 			x.innerHTML = "You're here!<br>" + 
 			"latitude: " + my_position.coords.latitude + 
 			"<br>longitude: " + my_position.coords.longitude +
 			"<br>accuracy: " + my_position.coords.accuracy +
-			"<br>time: " + my_position.timestamp;
-			var params = vN_LA + vD_LA + "&" + vN_LO + vD_LO + "&" + vN_A + vD_A + "&" + vN_T + vD_T;
+			"<br>time: " + my_position.timestamp +
+			"<br>altitude: " + vD_AL +
+			"<br>altitude accuracy: " + vD_AA + 
+			"<br>gender: " + vD_G +
+			"<br>handicap access: " + vD_H +
+			"<br>floor: " + vD_F;
+			
+			
+			/*var params = vN_LA + vD_LA + "&" + vN_LO + vD_LO + "&" + vN_A + vD_A + "&" + vN_T + vD_T;
 			xmlhttp.open("POST", url, true);
 
 			//This is what the php documentation page said to use for request headers. I have no clue what they do.
@@ -66,7 +94,7 @@ function findMe(){
 					alert(xmlhttp.responseText);
 				}
 			}
-			xmlhttp.send(params);
+			xmlhttp.send(params);*/
 		});
 	}
 	else {
